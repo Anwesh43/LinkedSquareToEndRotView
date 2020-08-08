10c56463720031913627c218d99818cae2ece1f7
Anwesh43 <anweshthecool0@gmail.com>
2020-08-09 04:07:50 +0530

    adding constants and libraries

--- squaretoendrotview/src/main/java/com/anwesh/uiprojects/squaretoendrotview/SquareToEndRotView.kt
+++ squaretoendrotview/src/main/java/com/anwesh/uiprojects/squaretoendrotview/SquareToEndRotView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.squaretoendrotview
 /**
  * Created by anweshmishra on 09/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf("#4CAF50", "#F44336", "#3F51B5", "#FFEB3B", "#2196F3")
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 5.1f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
